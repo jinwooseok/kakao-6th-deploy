91de603f4a73c23c39fd3950b0811c54008507a6
jinwooseok <jinus7949@naver.com>
2023-08-02 21:12:21 +0900

    test 및 api doc 생성

--- src/main/java/com/example/kakao/cart/CartRestController.java
+++ src/main/java/com/example/kakao/cart/CartRestController.java
@@ -17,26 +17,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class CartRestController {
+    private final CartService cartService;
 
-    private final CartService cartListService;
-
-    /**
-     * [
-     * {
-     * "optionId":1,
-     * "quantity":5
-     * },
-     * {
-     * "optionId":2,
-     * "quantity":5
-     * }
-     * ]
-     */
-    // (기능6) 장바구니 담기 POST
-    // /carts/add
     @PostMapping("/carts/add")
     public ResponseEntity<?> addCartList(@RequestBody @Valid List<CartRequest.SaveDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        cartListService.addCartList(requestDTOs, userDetails.getUser());
+        cartService.addCartList(requestDTOs, userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(null);
         return ResponseEntity.ok(apiResult);
     }
@@ -45,36 +30,16 @@ public class CartRestController {
     // /carts
     @GetMapping("/carts")
     public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        CartResponse.FindAllDTO responseDTO = cartListService.findAll(userDetails.getUser());
+        CartResponse.FindAllDTO responseDTO = cartService.findAll(userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
     }
 
-    @GetMapping("/carts/v2")
-    public ResponseEntity<?> findAllv2(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        CartResponse.FindAllDTOv2 responseDTO = cartListService.findAllv2(userDetails.getUser());
-        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
-        return ResponseEntity.ok(apiResult);
-    }
-
-
-    /**
-     * [
-     * {
-     * "cartId":1,
-     * "quantity":10
-     * },
-     * {
-     * "cartId":2,
-     * "quantity":10
-     * }
-     * ]
-     */
     // (기능8) 주문하기 - (주문화면에서 장바구니 수정하기)
-    // /carts/update
+// /carts/update
     @PostMapping("/carts/update")
     public ResponseEntity<?> update(@RequestBody @Valid List<CartRequest.UpdateDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        CartResponse.UpdateDTO responseDTO = cartListService.update(requestDTOs, userDetails.getUser());
+        CartResponse.UpdateDTO responseDTO = cartService.update(requestDTOs,userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
     }
--- src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -10,9 +10,7 @@ import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
 
-    @Query("select o from Option o where o.product.id = :productId")
-    List<Option> findByProductId(@Param("productId") int productId);
-
+    //option과 함께 product 정보들도 포함시켜야 하기 때문에 join fetch 전략을 사용한다.
     @Query("select o from Option o join fetch o.product where o.product.id = :productId")
-    List<Option> findByProductIdJoinProduct(@Param("productId") int productId);
+    List<Option> findByProductId(@Param("productId") int productId);
 }
